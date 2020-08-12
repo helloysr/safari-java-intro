c46f5d5b4699f8fa78cd7ebf3689f65bfd2f513b
Simon Roberts <simon@dancingcloudservices.com>
2020-08-12 07:16:40 -0600

    Break One

---
+++ src/main/java/hello/Bonjour.java
@@ -0,0 +1,15 @@
+// packages, dotted, lowercase, MUST match directory containing this source file
+package hello;
+
+// *public* class MUST be in a file that matches the class name
+public class Bonjour {
+  // template (not ready to discuss details!)
+  // main is starting point for Java code
+  public static void main(String [] args) {
+    // what will do the printing?
+    // "The standard output of the system"
+    // (could be something else)
+    // dotted left to right x.y "inside of x, find y"
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/main/java/operatorsetc/Ex1.java
@@ -0,0 +1,24 @@
+package operatorsetc;
+
+public class Ex1 {
+  public static void main(String[] args) {
+    int count;
+    count = 3;
+//    System.out.print("The count is ");
+//    System.out.println(count);
+
+    // + - * /
+    count = 6 + count;
+    // JAva will NOT lose meaning in assignment by assigning to "smaller" data
+    // Must force with a "cast" if you really want to
+//    int divided = (int)(count / 4.0);
+    double divided = count / 4.0;
+    System.out.println("The count is " + count);
+    System.out.println("The result of division is " + divided);
+
+    String message = "Hello, I am a piece of text";
+    String otherMessage = message.toUpperCase();
+    System.out.println(message);
+    System.out.println(otherMessage);
+  }
+}
